671e73d46041eb7c6232d33605bdb7a7138fff60
Noah Day <sheltielover432@gmail.com>
2021-04-27 00:28:22 -0400

    Main Menu Setup

---
+++ MainMenu/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField] private bool shouldShowOptionButton = true;
+    [SerializeField] private GameObject OptionsButton = null;
+    [SerializeField] private string levelToLoad = "";
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowGameObjects();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void ShowGameObjects()
+    {
+        if (shouldShowOptionButton == false)
+        {
+            OptionsButton.SetActive(false);
+        }
+    }
+
+    public void StartGame()
+    {
+        if (levelToLoad != "")
+        {
+            SceneManager.LoadScene(levelToLoad);
+        }
+        SceneManager.LoadScene(1);
+    }
+
+    public void OpenOptionsMenu()
+    {
+        
+    }
+
+    public void CloseOptions()
+    {
+        
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
