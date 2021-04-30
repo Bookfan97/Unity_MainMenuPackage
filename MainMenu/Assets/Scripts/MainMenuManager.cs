4ec9549a1d79629a235ec61540a5cab5f712c256
Noah Day <sheltielover432@gmail.com>
2021-04-30 03:18:17 -0400

    Graphics Settings

--- MainMenu/Assets/Scripts/MainMenuManager.cs
+++ MainMenu/Assets/Scripts/MainMenuManager.cs
@@ -1,76 +1,225 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
-    [Header("Toggles")]
-    [SerializeField] private bool shouldShowOption = true;
-    [SerializeField] private bool shouldGraphicsOptions = true;
-    [SerializeField] private bool shouldAudioOptions = true;
-    [Header("Main Menu")]
-    [SerializeField] private GameObject OptionsButton = null;
-    [SerializeField] private GameObject optionsScreen = null;
-    [SerializeField] private string levelToLoad = "";
-    [Header("Options Menu")]
-    [SerializeField] private GameObject graphicsLabel = null;
-    [SerializeField] private GameObject graphicsOptions = null;
-    [SerializeField] private GameObject graphicsApplyButton = null;
-    [SerializeField] private GameObject audioLabel = null;
-    [SerializeField] private GameObject audioOptions = null;
+    public ToggleClass Toggles;
+    public MainMenuClass MainMenu;
+    public OptionsMenuClass OptionsMenu;
+    public readonly ResolutionValues[] resolutions = {
+        new ResolutionValues(1920, 1080),
+        new ResolutionValues(1280, 720),
+        new ResolutionValues(854, 480),
+    };
     
+    private int selectedResolution;
+
+    #region VariableClasses
+    [Serializable]
+    public class ToggleClass
+    {
+        public bool ShouldShowOption = true;
+        public bool ShouldShowGraphicsOptions = true;
+        public bool ShouldShowAudioOptions = true;
+        public bool ShouldLoopResolutions = true;
+    }
+
+    [Serializable]
+    public class MainMenuClass
+    {
+        public GameObject OptionsButton = null;
+        public GameObject optionsScreen = null;
+        public string levelToLoad = "";
+    }
+
+    [Serializable]
+    public class OptionsMenuClass
+    {
+        public GameObject graphicsLabel = null;
+        public GameObject graphicsOptions = null;
+        public Text resolutionButtonText = null;
+        public GameObject graphicsApplyButton = null;
+        public GameObject audioLabel = null;
+        public GameObject audioOptions = null;
+        public Toggle VSyncToggle, FullscreenToggle = null;
+    }
+    
+    [Serializable]
+    public class ResolutionValues
+    {
+        public int Width, Height;
+
+        public ResolutionValues(int w, int h)
+        {
+            Width = w;
+            Height = h;
+        }
+    }
+    #endregion
+
+    #region Init
     // Start is called before the first frame update
     void Start()
     {
         ShowGameObjects();
-        optionsScreen.SetActive(false);
+        InitializeMenus();
     }
-    
-    // Update is called once per frame
-    void Update()
+
+    private void InitializeMenus()
     {
+        MainMenu.optionsScreen.SetActive(false);
         
+        //Graphics Settings
+        OptionsMenu.FullscreenToggle.isOn = Screen.fullScreen;
+        if (QualitySettings.vSyncCount == 0)
+        {
+            OptionsMenu.VSyncToggle.isOn = false;
+        }
+        else
+        {
+            OptionsMenu.VSyncToggle.isOn = true;
+        }
+        
+        //Resolution settings
+        bool foundResolution = false;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            if (Screen.width == resolutions[i].Width && Screen.height == resolutions[i].Height)
+            {
+                foundResolution = true;
+                selectedResolution = i;
+                ResolutionTextUpdate();
+            }
+        }
+
+        if (!foundResolution)
+        {
+            OptionsMenu.resolutionButtonText.text = Screen.width + " X " + Screen.height;
+        }
     }
 
     private void ShowGameObjects()
     {
-        OptionsButton.SetActive(shouldShowOption);
-        graphicsLabel.SetActive(shouldGraphicsOptions);
-        graphicsOptions.SetActive(shouldGraphicsOptions);
-        graphicsApplyButton.SetActive(shouldGraphicsOptions);
-        audioLabel.SetActive(shouldAudioOptions);
-        audioOptions.SetActive(shouldAudioOptions);
+        MainMenu.OptionsButton.SetActive(Toggles.ShouldShowOption);
+        OptionsMenu.graphicsLabel.SetActive(Toggles.ShouldShowGraphicsOptions);
+        OptionsMenu.graphicsOptions.SetActive(Toggles.ShouldShowGraphicsOptions);
+        OptionsMenu.graphicsApplyButton.SetActive(Toggles.ShouldShowGraphicsOptions);
+        OptionsMenu.audioLabel.SetActive(Toggles.ShouldShowAudioOptions);
+        OptionsMenu.audioOptions.SetActive(Toggles.ShouldShowAudioOptions);
     }
+    #endregion
 
+    #region MainMenu
     public void StartGame()
     {
-        if (levelToLoad != "")
+        if (MainMenu.levelToLoad != "")
         {
-            SceneManager.LoadScene(levelToLoad);
+            SceneManager.LoadScene(MainMenu.levelToLoad);
         }
         SceneManager.LoadScene(1);
     }
 
     public void OpenOptionsMenu()
     {
-        if (optionsScreen.gameObject.activeInHierarchy)
+        if (MainMenu.optionsScreen.activeInHierarchy == false)
         {
-            optionsScreen.SetActive(false);
-        }
-        else
-        {
-            optionsScreen.SetActive(true);
+            MainMenu.optionsScreen.SetActive(true);
         }
     }
 
     public void CloseOptions()
     {
-        
+        if (MainMenu.optionsScreen.activeInHierarchy)
+        {
+            MainMenu.optionsScreen.SetActive(false);
+        }
     }
 
     public void QuitGame()
     {
         Application.Quit();
     }
+    #endregion
+
+    #region OptionsMenu
+
+    public void ResolutionLeftButton()
+    {
+        if (Toggles.ShouldLoopResolutions)
+        {
+            if ((selectedResolution - 1) == 0 )
+            {
+                selectedResolution = resolutions.Length - 1;
+            }
+            else
+            {
+                selectedResolution--;
+            }
+        }
+        else
+        {
+            selectedResolution--;
+            if (selectedResolution < 0 )
+            {
+                selectedResolution = 0;
+            }
+        }
+
+        ResolutionTextUpdate();
+    }
+    
+    public void ResolutionRightButton()
+    {
+        if (Toggles.ShouldLoopResolutions)
+        {
+            if ((selectedResolution + 1) == resolutions.Length - 1)
+            {
+                selectedResolution = 0;
+            }
+            else
+            {
+                selectedResolution++;
+            }
+        }
+        else
+        {
+            selectedResolution++;
+            if (selectedResolution > resolutions.Length - 1)
+            {
+                selectedResolution = resolutions.Length - 1;
+            }
+        }
+        ResolutionTextUpdate();
+    }
+    
+    private void ResolutionTextUpdate()
+    {
+        OptionsMenu.resolutionButtonText.text = resolutions[selectedResolution].Width + " X " +
+                                                 resolutions[selectedResolution].Height;
+    }
+    
+    public void ApplyGraphics()
+    {
+        //Apply VSync
+        if (OptionsMenu.VSyncToggle.isOn)
+        {
+            QualitySettings.vSyncCount = 1;
+        }
+        else
+        {
+            QualitySettings.vSyncCount = 0;
+        }
+        
+        //Apply Resolution
+        Screen.SetResolution(
+            resolutions[selectedResolution].Width,
+            resolutions[selectedResolution].Height,
+                OptionsMenu.FullscreenToggle.isOn
+            );
+    }
+    #endregion
 }
+
