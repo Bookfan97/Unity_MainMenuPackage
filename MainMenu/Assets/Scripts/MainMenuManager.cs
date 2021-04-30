163a08703a320cfee2aeb92a0642aa1845838800
Noah Day <sheltielover432@gmail.com>
2021-04-29 23:59:45 -0400

    Options Label Layout

--- MainMenu/Assets/Scripts/MainMenuManager.cs
+++ MainMenu/Assets/Scripts/MainMenuManager.cs
@@ -7,12 +7,14 @@ public class MainMenuManager : MonoBehaviour
 {
     [SerializeField] private bool shouldShowOptionButton = true;
     [SerializeField] private GameObject OptionsButton = null;
+    [SerializeField] private GameObject optionsScreen = null;
     [SerializeField] private string levelToLoad = "";
     
     // Start is called before the first frame update
     void Start()
     {
         ShowGameObjects();
+        optionsScreen.SetActive(false);
     }
     
     // Update is called once per frame
@@ -40,7 +42,14 @@ public class MainMenuManager : MonoBehaviour
 
     public void OpenOptionsMenu()
     {
-        
+        if (optionsScreen.gameObject.activeInHierarchy)
+        {
+            optionsScreen.SetActive(false);
+        }
+        else
+        {
+            optionsScreen.SetActive(true);
+        }
     }
 
     public void CloseOptions()
